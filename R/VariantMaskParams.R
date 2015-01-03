9f817c756ef4d81c43d5294c30c2d335690fd423
Dan Bottomly <bottomly@ohsu.edu>
2015-01-03 08:57:26 -0800

    continuing with refactoring
    Added VariantMaskParams to the methods for searchCol, Table, Dict
    Close to finishing getProbeDf

--- R/VariantMaskParams.R
+++ R/VariantMaskParams.R
@@ -94,6 +94,21 @@ VariantMaskParams <- function(var.db, geno.filter=FALSE, rm.unmap=TRUE, rm.mult=
     return(new("VariantMaskParams", var.db=var.db, geno.filter=geno.filter, rm.unmap=rm.unmap, rm.mult=rm.mult, mask.type=mask.type))
 }
 
+setMethod("searchTables", signature("VariantMaskParams"), function(obj, name)
+          {
+            searchTables(obj@var.db, name=name)
+          })
+
+setMethod("searchCols", signature("VariantMaskParams"), function(obj, name, include.table=F)
+          {
+            searchCols(obj@var.db, name=name, include.table=include.table)
+          })
+
+setMethod("searchDict", signature("VariantMaskParams"), function(obj, name, value=NULL)
+          {
+            searchDict(obj@var.db, name=name, value=value)
+          })
+
 #should be replaced with poplite functionality...
 #get.shortest.query.path <- function(var.mask.par, start=NULL, finish=NULL, reverse=TRUE, undirected=TRUE)
 #{
@@ -276,6 +291,39 @@ dict.to.where <- function(object, dict.name, values)
 #    return(query.joins)
 #}
 
+#what the method is now...
+#object="GeneFeatureSet"
+#function (object, ...) 
+#{
+#    .local <- function (object, background = TRUE, normalize = TRUE, 
+#        subset = NULL, target = "core") 
+#    {
+#        target <- match.arg(target, c("core", "probeset"))
+#        featureInfo <- stArrayPmInfo(object, target = target)
+#        theClass <- class(exprs(object))
+#        pmi <- featureInfo[["fid"]]
+#        pnVec <- as.character(featureInfo[["fsetid"]])
+#        if ("matrix" %in% theClass) {
+#            pms <- exprs(object)[pmi, , drop = FALSE]
+#            dimnames(pms) <- NULL
+#            colnames(pms) <- sampleNames(object)
+#            theExprs <- basicRMA(pms, pnVec, normalize, background)
+#            rm(pms)
+#        }
+
+#with:
+
+#oligo:::stArrayPmInfo
+#function (object, target = "core", sortBy = "fsetid") 
+#{
+#    target <- match.arg(target, c("probeset", "core", "full", 
+#        "extended"))
+#    theFun <- switch(target, probeset = getFidProbeset, core = getFidMetaProbesetCore, 
+#        full = getFidMetaProbesetFull, extended = getFidMetaProbesetExtended)
+#    theFun(object, sortBy = sortBy)
+#}
+
+
 setGeneric("maskRMA", def=function(object, ...) standardGeneric("maskRMA"))
 setMethod("maskRMA", signature("GeneFeatureSet"), function(object, background=TRUE, normalize=TRUE, subset=NULL, target="core", mask.type=c("before.rma", "before.summary"), apply.mask=FALSE, mask.params=NULL)
           {
@@ -378,76 +426,78 @@ setMethod("getProbeDf", signature("VariantMaskParams"), function(object, gene.fs
     #something like below
     #all.db <- select(test.db, probe_id, align_status, reference.ref_id, filter)
     
-    #something like this...
-    #need to find a way to specifically reference the tables though as ref_id is in multiple tables...
     all.db <- select_(object@var.db, object@var.db@var.mask.probe.id,
                       object@var.db@var.mask.var.id,
-                      setNames(searchCols(object@var.db, "genotype.filter", include.table=T), NULL),
-                      setNames(searchCols(obj=object@var.db, name="mapping.status", include.table=T), NULL))
+                      setNames(searchCols(object,  name="genotype.filter", include.table=T), NULL),
+                      setNames(searchCols(object,  name="mapping.status", include.table=T), NULL))
     
+    var.only <- sapply(strsplit(object@var.db@var.mask.var.id, "\\."), "[", 2)
+    probe.only <- sapply(strsplit(object@var.db@var.mask.probe.id, "\\."), "[", 2)
     
-    geno.counts <- paste("n(", object@var.db@var.mask.var.id,") > 0")
+    geno.counts <- paste("n(", var.only ,") > 0")
 
     geno.filt <- paste("sum(IFNULL(",searchCols(object, "genotype.filter"),dict.to.where(object, dict.name="genotype.filter", values="TRUE"), ",0)) > 0")
     
-    #searchCols(obj=object, name="mapping.status")
-    #"align_status"
+    probe.group <- group_by_(all.db, probe.only, searchCols(obj=object, name="mapping.status"))
     
-    temp <- group_by_(all.db, test.db@var.mask.probe.id)
-    
-    temp.sum <- summarize_(temp, searchCols(obj=object, name="mapping.status"), .dots=setNames(list(geno.counts, geno.filt), c(var.presence, filter.presence)))
+    sum.by.probe <- summarize_(probe.group, .dots=setNames(list(geno.counts, geno.filt), c(var.presence, filter.presence)))
     
     #then add in the actual filters as below:
     
     if (object@geno.filter == TRUE)
     {
 #       #filter the result to only those with a filter value of TRUE, for this a NATURAL JOIN would suffice as we are filtering down
-#                    
-#                   where.base <- paste("WHERE (", var.presence, "= 1 AND", filter.presence, "= 1 AND", searchCols(obj=object@var.db, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"), ")")
-    }
-#                else
-#                {
-#                    where.base <- paste("WHERE (", var.presence, "= 1 AND", searchCols(obj=object@var.db, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"), ")")
-#                }
-#                
-#                outer.query <- paste(outer.query, where.base)
-#                
-#                if ((object@rm.unmap == TRUE || object@rm.mult == TRUE))
-#                {
-#                    if ((object@rm.unmap == TRUE && object@rm.mult == TRUE))
-#                    {
-#                        where.suff <- dict.to.where(object, "mapping.status", c("multi", "non"))
-#                    }
-#                    else if (object@rm.unmap == FALSE && object@rm.mult == TRUE)
-#                    {
-#                        where.suff <- dict.to.where(object, "mapping.status", "multi")
-#                    }
-#                    else if (object@rm.unmap == TRUE && object@rm.mult == FALSE)
-#                    {
-#                        where.suff <- dict.to.where(object, "mapping.status", "non")
-#                    }
-#                    else
-#                    {
-#                        stop("ERROR: rm.unmap and rm.mult should not be false here")
-#                    }
-#                    
-#                    outer.query <- paste(outer.query, "OR (", searchCols(obj=object@var.db, name="mapping.status"), where.suff, ")")
-#                }
-    
-    temp.sum.filt <- filter_(temp.sum, paste(paste(var.presence, "== 1"), "&",
+                    
+        #where.base <- paste("WHERE (", var.presence, "= 1 AND", filter.presence, "= 1 AND", searchCols(obj=object@var.db, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"), ")")
+        probe.sum.filt <- filter_(temp.sum, paste(paste(var.presence, "== 1"), "&",
                                              paste(filter.presence, "== 1"), "&",
                                              paste(searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"))
                                              ))
-    
-    mapping.query <- paste( searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", c("multi", "non")))
+    }
+    else
+    {
+        #where.base <- paste("WHERE (", var.presence, "= 1 AND", searchCols(obj=object@var.db, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"), ")")
+        probe.sum.filt <- filter_(temp.sum, paste(paste(var.presence, "== 1"), "&",
+                                             paste(searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", "unique"))
+                                             ))
+    }
+#                
+#                outer.query <- paste(outer.query, where.base)
+#                
+    if ((object@rm.unmap == TRUE || object@rm.mult == TRUE))
+    {
+        if ((object@rm.unmap == TRUE && object@rm.mult == TRUE))
+        {
+            #where.suff <- dict.to.where(object, "mapping.status", c("multi", "non"))
+            mapping.query <- paste( searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", c("multi", "non")))
+        }
+        else if (object@rm.unmap == FALSE && object@rm.mult == TRUE)
+        {
+            #where.suff <- dict.to.where(object, "mapping.status", "multi")
+            mapping.query <- paste( searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", "multi"))
+        }
+        else if (object@rm.unmap == TRUE && object@rm.mult == FALSE)
+        {
+            #where.suff <- dict.to.where(object, "mapping.status", "non")
+            mapping.query<- paste( searchCols(obj=object, name="mapping.status"), dict.to.where(object, "mapping.status", "non"))
+        }
+        else
+        {
+            stop("ERROR: rm.unmap and rm.mult should not be false here")
+        }
+        
+        #outer.query <- paste(outer.query, "OR (", searchCols(obj=object@var.db, name="mapping.status"), where.suff, ")")
+        
+        all.rm.probes <- union(select_(probe.sum.filt, probe.only), select_(filter_(select(object@var.db, .tables="probe_info"), mapping.query), probe.only), copy=T)
+        
+    }else{
+        
+        all.rm.probes <- select_(probe.sum.filt, probe.only)
+    }
 
-    all.probes <- union(select_(temp.sum.filt, object@var.mask.probe.id), select_(filter_(select(test.db, .tables="probe_info"), mapping.query), object@var.mask.probe.id), copy=T)
-
-
-    
     #get the probesets from oligo
     
-    
+    getProbeInfo(object, field, probeType = "pm", target = "core")
     
     
 })
